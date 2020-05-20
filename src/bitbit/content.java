b00b597f4ee603b83e73606bec40c6f3c7ed35ee
kooeunhyeon <dmsgus0429@naver.com>
2020-05-20 16:40:03 +0900

    insert 병합 - 은현 - content.java 쉊ㅇ

--- src/bitbit/content.java
+++ src/bitbit/content.java
@@ -4,5 +4,6 @@ public class content {
 	public static void main(String[] args) {
 		System.out.println("bitbit start");
 		System.out.println("bitbit loading...?!");
+		System.out.println("bitbit insert");
 	}
 }
